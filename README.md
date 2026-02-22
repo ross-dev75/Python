e586b67f9da0568a4a9cc81070008abee2169705
Ryan <260121549+ross-dev75@users.noreply.github.com>
2026-02-22 15:40:27 -0600

    Day 01: Basic input/output and variables practice

---
+++ README.md
@@ -0,0 +1,6 @@
+100 Days of Python!
+
+Daily Python Practice from the ground up.
+
+## Day 01
+Basic input/output and variable practice.
---
+++ day01.py
@@ -0,0 +1,4 @@
+city = input("What was the name of the city you grew up in?\n")
+pets_name = input("What is the name of your pet?\n")
+band_name = city + " " + pets_name
+print("Your band name could be " + band_name)
